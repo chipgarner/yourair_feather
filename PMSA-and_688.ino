b5a3ef4044413ed610d57111abb2d855f1a65b73
James Kirkland Garner <chip@sentryai.com>
2022-08-17 09:53:01 -0700

    first commit

---
+++ PMSA-and_688.ino
@@ -0,0 +1,217 @@
+#include <Adafruit_GrayOLED.h>
+#include <gfxfont.h>
+#include <Adafruit_SPITFT.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SPITFT_Macros.h>
+
+#include <Adafruit_SSD1306.h>
+#include <splash.h>
+
+/***************************************************************************
+  This is a library for the BME680 gas, humidity, temperature & pressure sensor
+
+  Designed specifically to work with the Adafruit BME680 Breakout
+  ----> http://www.adafruit.com/products/3660
+
+  These sensors use I2C or SPI to communicate, 2 or 4 pins are required
+  to interface.
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing products
+  from Adafruit!
+
+  Written by Limor Fried & Kevin Townsend for Adafruit Industries.
+  BSD license, all text above must be included in any redistribution
+ ***************************************************************************/
+
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BME680.h"
+#include "Adafruit_PM25AQI.h"
+
+#define BME_SCK 13
+#define BME_MISO 12
+#define BME_MOSI 11
+#define BME_CS 10
+
+#define SEALEVELPRESSURE_HPA (1020.3)
+// 1013.25 standard
+
+
+// OLED FeatherWing buttons map to different pins depending on board.
+// The I2C (Wire) bus may also be different.
+#if defined(ESP8266)
+  #define BUTTON_A  0
+  #define BUTTON_B 16
+  #define BUTTON_C  2
+  #define WIRE Wire
+#elif defined(ESP32)
+  #define BUTTON_A 15
+  #define BUTTON_B 32
+  #define BUTTON_C 14
+  #define WIRE Wire
+#elif defined(ARDUINO_STM32_FEATHER)
+  #define BUTTON_A PA15
+  #define BUTTON_B PC7
+  #define BUTTON_C PC5
+  #define WIRE Wire
+#elif defined(TEENSYDUINO)
+  #define BUTTON_A  4
+  #define BUTTON_B  3
+  #define BUTTON_C  8
+  #define WIRE Wire
+#elif defined(ARDUINO_FEATHER52832)
+  #define BUTTON_A 31
+  #define BUTTON_B 30
+  #define BUTTON_C 27
+  #define WIRE Wire
+#elif defined(ARDUINO_ADAFRUIT_FEATHER_RP2040)
+  #define BUTTON_A  9
+  #define BUTTON_B  8
+  #define BUTTON_C  7
+  #define WIRE Wire1
+#else // 32u4, M0, M4, nrf52840 and 328p
+  #define BUTTON_A  9
+  #define BUTTON_B  6
+  #define BUTTON_C  5
+  #define WIRE Wire
+#endif
+
+//#define WIRE Wire
+Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &WIRE);
+
+Adafruit_BME680 bme; // I2C
+Adafruit_PM25AQI aqi = Adafruit_PM25AQI();
+
+void setup() {
+  Serial.begin(57600);
+  while (!Serial) delay(50);
+
+  delay(3000); //Doesn't always work without a delay here.
+  
+  setup_oled_display();
+
+  if (!bme.begin()) {
+    Serial.println("Could not find a valid BME680 sensor, check wiring!");
+  }
+  else {
+    // Set up oversampling and filter initialization
+    bme.setTemperatureOversampling(BME680_OS_8X);
+    bme.setHumidityOversampling(BME680_OS_2X);
+    bme.setPressureOversampling(BME680_OS_4X);
+    bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+    bme.setGasHeater(320, 0); // 0 is off. e.g. 320*C for 150 ms
+
+    Serial.println(F("BME68x running!"));
+  }
+
+  if (! aqi.begin_I2C()) {      // connect to the PMSA sensor over I2C
+    Serial.println("Could not find PMSA0031 sensor!");
+  }
+  else {
+    Serial.println("Adafruit PMSA003I Air Quality Sensor running!");
+  }
+
+}
+
+
+void setup_oled_display() {
+
+  Serial.println("OLED FeatherWing test");
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // Address 0x3C for 128x32
+
+  Serial.println("OLED begun");
+
+  // Show image buffer on the display hardware.
+  // Since the buffer is intialized with an Adafruit splashscreen
+  // internally, this will display the splashscreen.
+  display.display();
+  delay(2000);
+
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0,0);
+  // Clear the buffer.
+  display.clearDisplay();
+  display.display();
+
+  Serial.println(F(" "));
+  Serial.println(F("BME680 setup"));
+
+}
+
+
+void loop() {
+  if (! bme.performReading()) {
+    Serial.println("Failed to perform reading :(");
+    delay(500);
+  }
+  Serial.print("Temperature = ");
+  Serial.print(bme.temperature);
+  Serial.println(" *C");
+
+  Serial.print("Pressure = ");
+  Serial.print(bme.pressure / 100.0);
+  Serial.println(" hPa");
+
+  Serial.print("Humidity = ");
+  Serial.print(bme.humidity);
+  Serial.println(" %");
+
+  Serial.print("Gas = ");
+  Serial.print(bme.gas_resistance / 1000.0);
+  Serial.println(" KOhms");
+
+  Serial.print("Approx. Altitude = ");
+  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println(" m");
+
+  Serial.println();
+
+  PM25_AQI_Data data;
+
+  if (! aqi.read(&data)) {
+    Serial.println("Could not read from AQI");
+    delay(500);  // try again in a bit!
+  }
+  Serial.println("AQI reading success");
+  
+  display.clearDisplay();
+  display.setCursor(0,0);
+  display.print("PM2.5: ");
+  display.print(data.pm25_env);
+  display.println(" ug/m^3");
+  display.print("Humidity: ");
+  display.print(bme.humidity);
+  display.println("%");
+  display.print("Altitude: ");
+  display.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+  display.println(" m");
+  display.print("Temperature: ");
+  display.print(bme.temperature);
+  display.print(" C");
+  display.display();
+
+  Serial.println();
+  Serial.println(F("---------------------------------------"));
+  Serial.println(F("Concentration Units (standard)"));
+  Serial.println(F("---------------------------------------"));
+  Serial.print(F("PM 1.0: ")); Serial.print(data.pm10_standard);
+  Serial.print(F("\t\tPM 2.5: ")); Serial.print(data.pm25_standard);
+  Serial.print(F("\t\tPM 10: ")); Serial.println(data.pm100_standard);
+  Serial.println(F("Concentration Units (environmental)"));
+  Serial.println(F("---------------------------------------"));
+  Serial.print(F("PM 1.0: ")); Serial.print(data.pm10_env);
+  Serial.print(F("\t\tPM 2.5: ")); Serial.print(data.pm25_env);
+  Serial.print(F("\t\tPM 10: ")); Serial.println(data.pm100_env);
+  Serial.println(F("---------------------------------------"));
+  Serial.print(F("Particles > 0.3um / 0.1L air:")); Serial.println(data.particles_03um);
+  Serial.print(F("Particles > 0.5um / 0.1L air:")); Serial.println(data.particles_05um);
+  Serial.print(F("Particles > 1.0um / 0.1L air:")); Serial.println(data.particles_10um);
+  Serial.print(F("Particles > 2.5um / 0.1L air:")); Serial.println(data.particles_25um);
+  Serial.print(F("Particles > 5.0um / 0.1L air:")); Serial.println(data.particles_50um);
+  Serial.print(F("Particles > 10 um / 0.1L air:")); Serial.println(data.particles_100um);
+  Serial.println(F("---------------------------------------"));
+}
